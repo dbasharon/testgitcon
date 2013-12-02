5a8b34ed332164f3b8924a2ed51aeb2459086eae
sharon <dbasharon@walla.co.il>
2013-12-02 17:42:54 +0200

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testgitcon::Application.config.secret_key_base = 'c8e082604f5bdffb721c9da71ca1bf5a3fac7f881a671956c461d4ce794cd03db384e6304260dbd8e0637c8ab3578117d905ee1354ae049ae3338f50c1360456'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testgitcon::Application.config.session_store :cookie_store, key: '_testgitcon_session'
